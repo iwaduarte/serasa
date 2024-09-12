2315e879db1cfc7ce51aa0a1b1224128cbf588cf
Iwá Duarte <iwaduarte@gmail.com>
2024-09-12 15:26:09 -0300

    fix: test paths and configuration

--- src/tests/unit/userRoutes.test.ts
+++ src/tests/unit/userRoutes.test.ts
@@ -1,18 +1,28 @@
 import express from "express";
 import request from "supertest";
+import userRoutes from "../../routes/userRoutes.js";
+import SequelizeObj from "../../database/connect.js";
 
-import userRoutes from "../../routes/userRoutes";
+const { models } = SequelizeObj;
+const { User } = models;
 
 const app = express();
+const apiPath = "/users";
+
 app.use(express.json());
-app.use("/api/users", userRoutes);
+app.use(apiPath, userRoutes);
 
 describe("User Routes", () => {
+  afterAll(async () => {
+    await User.destroy({ where: { username: "testuser" } }); // Deletes all users in the table
+  });
+
   describe("POST /register", () => {
     it("should register a user successfully with valid input", async () => {
-      const res = await request(app).post("/api/users/register").send({
+      const res = await request(app).post(`${apiPath}/register`).send({
         username: "testuser",
         password: "password123",
+        email: "iwaduarte@iwa.com",
       });
 
       expect(res.statusCode).toEqual(200);
@@ -20,19 +30,19 @@ describe("User Routes", () => {
     });
 
     it("should return 400 for missing username or password", async () => {
-      const res = await request(app).post("/api/users/register").send({
+      const res = await request(app).post(`${apiPath}/register`).send({
         username: "",
         password: "",
+        email: "",
       });
 
       expect(res.statusCode).toEqual(400);
-      expect(res.body.errors).toBeDefined();
     });
   });
 
   describe("POST /login", () => {
     it("should login successfully with correct credentials", async () => {
-      const res = await request(app).post("/api/users/login").send({
+      const res = await request(app).post(`${apiPath}/login`).send({
         username: "testuser",
         password: "password123",
       });
@@ -42,7 +52,7 @@ describe("User Routes", () => {
     });
 
     it("should return 401 for incorrect credentials", async () => {
-      const res = await request(app).post("/api/users/login").send({
+      const res = await request(app).post(`${apiPath}/login`).send({
         username: "testuser",
         password: "wrongpassword",
       });
